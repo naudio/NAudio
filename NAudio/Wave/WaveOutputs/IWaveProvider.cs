810e90275a3df5ea6adf3e30f7098c72a2e4c3c5
alexandre_mutel <alexandre_mutel@localhost>
2008-06-17 14:42:57 +0000

    Add WaveBuffer class and IWaveProvider interface for next NAudio architecture. Still in design mode, not yet plugged and used.

---
+++ NAudio/Wave/WaveOutputs/IWaveProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NAudio.Wave
+{
+    /// <summary>
+    /// Generic interface for all WaveProviders.
+    /// 
+    /// NOT YET USED.
+    /// </summary>
+    public interface IWaveProvider
+    {
+        /// <summary>
+        /// Gets the WaveFormat of this WaveProvider.
+        /// </summary>
+        /// <value>The wave format.</value>
+        WaveFormat WaveFormat { get; }
+
+        /// <summary>
+        /// Fill the specified buffer with wave data.
+        /// </summary>
+        /// <param name="buffer">The buffer to fill of wave data.</param>
+        /// <returns>the number of bytes written to the buffer.</returns>
+        int Read(WaveBuffer buffer);
+    }
+}

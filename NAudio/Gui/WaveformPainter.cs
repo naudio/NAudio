47b433e7b675124fb3d1d24c8f00445f4e6300e9
markheath <markheath@localhost>
2008-10-20 21:03:12 +0000

    Added new WaveformPainter and VolumeMeter WinForm controls for nice GUI feedback of volume level and waveform display. Demo added to the NAudioDemo project. Also made some possible bugfixes to ASIO out as suggested on CodePlex issue tracker

---
+++ NAudio/Gui/WaveformPainter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NAudio.Gui
+{
+    public partial class WaveformPainter : Control
+    {
+        Pen foregroundPen;
+        List<float> samples = new List<float>(1000);
+        int maxSamples;
+        int insertPos;
+
+        public WaveformPainter()
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint |
+                ControlStyles.OptimizedDoubleBuffer, true);
+            InitializeComponent();
+            OnForeColorChanged(EventArgs.Empty);
+            OnResize(EventArgs.Empty);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            maxSamples = this.Width;
+            base.OnResize(e);
+        }
+
+        protected override void OnForeColorChanged(EventArgs e)
+        {
+            foregroundPen = new Pen(ForeColor);
+            base.OnForeColorChanged(e);
+        }
+
+        public void AddMax(float maxSample)
+        {
+            if (samples.Count <= maxSamples)
+            {
+                samples.Add(maxSample);
+            }
+            else
+            {
+                samples[insertPos] = maxSample;
+            }
+            insertPos++;
+            insertPos %= maxSamples;
+            
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            for (int x = 0; x < this.Width; x++)
+            {                
+                float lineHeight = this.Height * GetSample(x - this.Width + insertPos);
+                float y1 = (this.Height - lineHeight) / 2;
+                pe.Graphics.DrawLine(foregroundPen, x, y1, x, y1 + lineHeight);
+            }
+        }
+
+        float GetSample(int index)
+        {
+            if (index < 0)
+                index += maxSamples;
+            if (index >= 0 & index < samples.Count)
+                return samples[index];
+            return 0;
+        }
+    }
+}
---
+++ NAudioDemo/MeteringStream.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NAudio.Wave;
+
+namespace NAudioDemo
+{
+    /// <summary>
+    /// basic metering stream
+    /// n.b. does not close its source stream
+    /// </summary>
+    public class MeteringStream : WaveStream
+    {
+        public WaveStream SourceStream { get; private set; }
+        public int SamplesPerNotification { get; set; }
+
+        float[] maxSamples;
+        int sampleCount;
+
+        public event EventHandler<StreamVolumeEventArgs> StreamVolume;
+
+        public MeteringStream(WaveStream sourceStream) :
+            this(sourceStream, sourceStream.WaveFormat.SampleRate / 10)
+        {
+        }
+
+        public MeteringStream(WaveStream sourceStream, int samplesPerNotification)
+        {
+            SourceStream = sourceStream;
+            if (sourceStream.WaveFormat.BitsPerSample != 32)
+                throw new ArgumentException("Metering Stream expects 32 bit floating point audio", "sourceStream");
+            maxSamples = new float[sourceStream.WaveFormat.Channels];
+            this.SamplesPerNotification = samplesPerNotification;
+        }
+
+        public override WaveFormat WaveFormat
+        {
+            get { return SourceStream.WaveFormat; }
+        }
+
+        public override long Length
+        {
+            get { return SourceStream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return SourceStream.Position; }
+            set { SourceStream.Position = value; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {           
+            int bytesRead = SourceStream.Read(buffer, offset, count);
+            ProcessData(buffer, offset, bytesRead);
+            return bytesRead;
+        }
+
+        private void ProcessData(byte[] buffer, int offset, int count)
+        {
+            int index = 0;
+            while ( index < count)
+            {
+                for (int channel = 0; channel < maxSamples.Length; channel++)
+                {
+                    float sampleValue = Math.Abs(BitConverter.ToSingle(buffer, offset + index));
+                    maxSamples[channel] = Math.Max(maxSamples[channel],sampleValue);
+                    index += 4;
+                }
+                sampleCount++;
+                if(sampleCount >= SamplesPerNotification)
+                {
+                    RaiseStreamVolumeNotification();
+                    sampleCount = 0;
+                    Array.Clear(maxSamples, 0, maxSamples.Length);
+                    
+                }
+                
+            }
+        }
+
+        private void RaiseStreamVolumeNotification()
+        {
+            if (StreamVolume != null)
+            {
+                StreamVolume(this, new StreamVolumeEventArgs() { MaxSampleValues = (float[])maxSamples.Clone() });
+            }
+        }
+    }
+
+    public class StreamVolumeEventArgs : EventArgs
+    {
+        public float[] MaxSampleValues { get; set; }
+    }
+}

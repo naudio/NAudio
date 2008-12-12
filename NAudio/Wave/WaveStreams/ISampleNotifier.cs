bc45bf25b45be1d26e66a84fe2a2787bb9fe1dfc
markheath <mark.heath@gmail.com>
2008-12-12 22:52:09 +0000

    oops last checkin broke build. Adding ISampleNotifier but this is strictly WIP at the moment

---
+++ NAudio/Wave/WaveStreams/ISampleNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NAudio.Wave
+{
+    public class ISampleNotifier
+    {
+        public event EventHandler Block;
+        public event EventHandler<SampleEventArgs> Sample;
+    }
+
+    public class SampleEventArgs : EventArgs
+    {
+        public float left;
+        public float right;
+    }
+}

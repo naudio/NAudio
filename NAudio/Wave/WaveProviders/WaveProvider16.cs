ce3eeaa936f96c59b4af7acd458aa73fea644b15
markheath <markheath@localhost>
2009-10-08 12:24:57 +0000

    fixed an incorrect namespace

--- NAudio/Wave/WaveProviders/WaveProvider16.cs
+++ NAudio/Wave/WaveProviders/WaveProvider16.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NAudio.Wave.WaveProviders
+namespace NAudio.Wave
 {
     /// <summary>
     /// Base class for creating a 16 bit wave provider

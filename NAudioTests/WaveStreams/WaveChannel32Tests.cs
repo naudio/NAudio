cd426239b35f39a2adf5549f14e02cb7ddc6b4ed
markheath <markheath@localhost>
2010-01-04 10:52:47 +0000

    fixing a bug with WaveChannel32 making it unsuitable for use with WaveFileWriter.CreateWaveFile

---
+++ NAudioTests/WaveStreams/WaveChannel32Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NAudio.Wave;
+using NAudioTests.Utils;
+
+namespace NAudioTests.WaveStreams
+{
+    [TestFixture]
+    public class WaveChannel32Tests
+    {
+        [Test]
+        public void CanCreateWavFileFromWaveChannel32()
+        {
+            string inFile = @"F:\Recording\wav\pcm\16bit mono 8kHz.wav";
+            string outFile = @"F:\Recording\wav\pcm\32bit stereo 8kHz.wav";
+            var audio32 = new WaveChannel32(new WaveFileReader(inFile));
+            audio32.PadWithZeroes = false;
+            WaveFileWriter.CreateWaveFile(outFile, audio32);
+        }
+    }
+}

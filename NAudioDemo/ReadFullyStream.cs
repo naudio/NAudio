d2c3cad4c5317e173424cb3a85af858dad3753e8
markheath <mark.heath@gmail.com>
2011-05-02 09:32:41 +0000

    MP3 Streaming example now allows you to pause to buffer ahead, and gives a visual indication of amount of data buffered

--- NAudioDemo/ReadFullyStream.cs
+++ NAudioDemo/ReadFullyStream.cs
@@ -76,7 +76,7 @@ namespace NAudioDemo
                 {
                     readAheadOffset = 0;
                     readAheadLength = sourceStream.Read(readAheadBuffer, 0, readAheadBuffer.Length);
-                    Debug.WriteLine(String.Format("Read {0} bytes (requested {1})", readAheadLength, readAheadBuffer.Length));
+                    //Debug.WriteLine(String.Format("Read {0} bytes (requested {1})", readAheadLength, readAheadBuffer.Length));
                     if (readAheadLength == 0)
                     {
                         break;

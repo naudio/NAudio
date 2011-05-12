197c70b0c51372e9de73cec9e7214a4e5a7f9fdb
markheath <mark.heath@gmail.com>
2011-05-12 15:25:14 +0000

    Microsoft ADPCM 32kbps added to Network Chat demo

---
+++ NAudioDemo/NetworkChatDemo/MicrosoftAdpcmChatCodec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+
+namespace NAudioDemo.NetworkChatDemo
+{
+    class MicrosoftAdpcmChatCodec : AcmChatCodec
+    {
+        public MicrosoftAdpcmChatCodec()
+            : base(new WaveFormat(8000, 16, 1), new AdpcmWaveFormat(8000,1))
+        {
+        }
+
+        public override string Name { get { return "Microsoft ADPCM"; } }
+    }
+}

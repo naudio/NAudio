13f548b56ad32d6a27368d578e5c5a45ce71d5e9
markheath <markheath@localhost>
2011-05-14 09:48:11 +0000

    NAudioDemo audio file playback demo now uses MEF for supported input files. Added support for AIFF.

---
+++ NAudioDemo/AudioPlaybackDemo/IInputFileFormatPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+
+namespace NAudioDemo.AudioPlaybackDemo
+{
+    public interface IInputFileFormatPlugin
+    {
+        string Name { get; }
+        string Extension { get; }
+        WaveStream CreateWaveStream(string fileName);
+    }
+}
---
+++ NAudioDemo/AudioPlaybackDemo/WaveInputFilePlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+using System.ComponentModel.Composition;
+
+namespace NAudioDemo.AudioPlaybackDemo
+{
+    [Export(typeof(IInputFileFormatPlugin))]
+    class WaveInputFilePlugin : IInputFileFormatPlugin
+    {
+        public string Name
+        {
+            get { return "WAV file"; }
+        }
+
+        public string Extension
+        {
+            get { return ".wav"; }
+        }
+
+        public WaveStream CreateWaveStream(string fileName)
+        {
+            WaveStream readerStream = new WaveFileReader(fileName);
+            if (readerStream.WaveFormat.Encoding != WaveFormatEncoding.Pcm && readerStream.WaveFormat.Encoding != WaveFormatEncoding.IeeeFloat)
+            {
+                readerStream = WaveFormatConversionStream.CreatePcmStream(readerStream);
+                readerStream = new BlockAlignReductionStream(readerStream);
+            }
+            return readerStream;
+        }
+    }
+}

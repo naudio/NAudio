a9b736152daa2f6a61e11a433e741186d47a1220
markheath <mark.heath@gmail.com>
2011-05-18 21:48:45 +0000

    Adding a new class AudioFileReader for simplified playback scenarios. Still a work in progress - need to scale the values returned by Length and Position for the WaveStream base class conversions to TimeSpan to work. Added a new panel to NAudioDemo to demonstrate its use and experiment with getting the PlaybackStopped event to be reliable. Will not be shown by default in NAudioDemo until it is more complete.

---
+++ NAudioDemo/SimplePlaybackDemo/SimplePlaybackPanel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.Diagnostics;
+
+namespace NAudioDemo.SimplePlaybackDemo
+{
+    public partial class SimplePlaybackPanel : UserControl
+    {
+        private IWavePlayer wavePlayer;
+        private WaveStream file;
+        private string fileName;
+
+        public SimplePlaybackPanel()
+        {
+            InitializeComponent();
+            EnableButtons(false);
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (SelectInputFile())
+            {
+                BeginPlayback(fileName);
+            }
+        }
+
+        private bool SelectInputFile()
+        {
+            if (fileName == null)
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Audio Files|*.mp3;*.wav;*.aiff";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    this.fileName = ofd.FileName;
+                }
+            }
+            return fileName != null;
+        }
+
+        private void BeginPlayback(string filename)
+        {
+            this.wavePlayer = new WaveOut();
+            this.file = new AudioFileReader(filename);
+            this.wavePlayer.Init(file);
+            this.wavePlayer.PlaybackStopped += new EventHandler(wavePlayer_PlaybackStopped);
+            this.wavePlayer.Play();
+            EnableButtons(true);
+        }
+
+        private void EnableButtons(bool playing)
+        {
+            buttonPlay.Enabled = !playing;
+            buttonStop.Enabled = playing;
+        }
+
+        void wavePlayer_PlaybackStopped(object sender, EventArgs e)
+        {
+            // we want it to be safe to clean up input stream and playback device in the handler for PlaybackStopped
+            CleanUp();
+            // we want to be always on the GUI thread and be able to change GUI components
+            Debug.Assert(!this.InvokeRequired, "PlaybackStopped on wrong thread");
+            EnableButtons(false);
+        }
+
+        private void CleanUp()
+        {
+            this.file.Dispose();
+            this.file = null;
+            this.wavePlayer.Dispose();
+            this.wavePlayer = null;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            this.wavePlayer.Stop();
+            // don't set button states now, we'll wait for our PlaybackStopped to come
+        }
+    }
+}

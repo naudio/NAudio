facd99e4b4f06031d340b3a4aa3021f772b8d241
markheath <mark.heath@gmail.com>
2011-05-19 06:07:26 +0000

    Improvements to WaveOut with function callbacks. Will attempt to always call PlaybackStopped from GUI thread if possible. Also, default constructor will use Function Callback if created from a non GUI thread. Added a volume control to the Simple Playback Panel

--- NAudioDemo/SimplePlaybackDemo/SimplePlaybackPanel.cs
+++ NAudioDemo/SimplePlaybackDemo/SimplePlaybackPanel.cs
@@ -14,13 +14,16 @@ namespace NAudioDemo.SimplePlaybackDemo
     public partial class SimplePlaybackPanel : UserControl
     {
         private IWavePlayer wavePlayer;
-        private WaveStream file;
+        private AudioFileReader file;
         private string fileName;
 
         public SimplePlaybackPanel()
         {
             InitializeComponent();
             EnableButtons(false);
+            comboBoxOutputDriver.Items.Add("WaveOut");
+            comboBoxOutputDriver.Items.Add("WaveOut Function Callbacks");
+            comboBoxOutputDriver.SelectedIndex = 0;
         }
 
         private void buttonPlay_Click(object sender, EventArgs e)
@@ -48,18 +51,33 @@ namespace NAudioDemo.SimplePlaybackDemo
 
         private void BeginPlayback(string filename)
         {
-            this.wavePlayer = new WaveOut();
+            Debug.Assert(this.wavePlayer == null);
+            this.wavePlayer = CreateWavePlayer();
             this.file = new AudioFileReader(filename);
+            this.file.Volume = volumeSlider1.Volume;
             this.wavePlayer.Init(file);
             this.wavePlayer.PlaybackStopped += new EventHandler(wavePlayer_PlaybackStopped);
             this.wavePlayer.Play();
             EnableButtons(true);
         }
 
+        private IWavePlayer CreateWavePlayer()
+        {
+            switch (comboBoxOutputDriver.SelectedIndex)
+            {
+                case 1:
+                    return new WaveOut(WaveCallbackInfo.FunctionCallback());
+                case 0:
+                default:
+                    return new WaveOut();
+            }
+        }
+
         private void EnableButtons(bool playing)
         {
             buttonPlay.Enabled = !playing;
             buttonStop.Enabled = playing;
+            buttonOpen.Enabled = !playing;
         }
 
         void wavePlayer_PlaybackStopped(object sender, EventArgs e)
@@ -83,6 +101,19 @@ namespace NAudioDemo.SimplePlaybackDemo
         {
             this.wavePlayer.Stop();
             // don't set button states now, we'll wait for our PlaybackStopped to come
+        }
+
+        private void volumeSlider1_VolumeChanged(object sender, EventArgs e)
+        {
+            if (this.file != null)
+            {
+                this.file.Volume = volumeSlider1.Volume;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            SelectInputFile();
         }
     }
 }

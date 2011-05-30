3dd1a067d50a47fc422ac6d5e112de2021c3d31a
markheath <mark.heath@gmail.com>
2011-05-30 22:16:52 +0000

    starting to add a basic drum machine demo for WPF. Still a work in progress - does not work yet.

---
+++ NAudioWpfDemo/DrumMachineDemo/DrumMachineDemoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace NAudioWpfDemo.DrumMachineDemo
+{
+    class DrumMachineDemoViewModel : IDisposable
+    {
+        private IWavePlayer waveOut;
+        public DrumMachineDemoViewModel()
+        {
+            PlayCommand = new RelayCommand(
+               () => this.Play(),
+               () => true);
+        }
+
+        public ICommand PlayCommand { get; private set; }
+
+        private void Play()
+        {
+            waveOut = new WaveOut();
+            ISampleProvider sp = new PatternSequencer();
+            IWaveProvider wp = new SampleToWaveProvider(sp);
+            waveOut.Init(wp);
+            waveOut.Play();
+        }
+
+        public void Dispose()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+    }
+}
---
+++ NAudioWpfDemo/DrumMachineDemo/SampleSource.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace NAudioWpfDemo.DrumMachineDemo
+{
+    class SampleSource
+    {
+        public static SampleSource CreateFromWaveFile(string fileName)
+        {
+            using (var reader = new WaveFileReader(fileName))
+            {
+                ISampleProvider sp;
+                int sourceSamples;
+                if (reader.WaveFormat.Encoding == WaveFormatEncoding.Pcm)
+                {
+                    if (reader.WaveFormat.BitsPerSample == 16)
+                    {
+                        sp = new Pcm16BitToSampleProvider(reader);
+                        sourceSamples = (int)(reader.Length / 2);
+                    }
+                    else if (reader.WaveFormat.BitsPerSample == 24)
+                    {
+                        sp = new Pcm24BitToSampleProvider(reader);
+                        sourceSamples = (int)(reader.Length / 3);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Currently only 16 or 24 bit PCM samples are supported");
+                    }
+                }
+                else if (reader.WaveFormat.Encoding == WaveFormatEncoding.IeeeFloat)
+                {
+                    sp = new WaveToSampleProvider(reader);
+                    sourceSamples = (int)(reader.Length / 4);
+                }
+                else
+                {
+                    throw new ArgumentException("Must be PCM or IEEE float");
+                }
+                float[] sampleData = new float[sourceSamples];
+                int n = sp.Read(sampleData, 0, sourceSamples);
+                if (n != sourceSamples)
+                {
+                    throw new InvalidOperationException(String.Format("Couldn't read the whole sample, expected {0} samples, got {1}", n, sourceSamples));
+                }
+                SampleSource ss = new SampleSource(sampleData, sp.WaveFormat);
+                return ss;
+            }
+        }
+
+        public SampleSource(float[] sampleData, WaveFormat waveFormat) :
+            this(sampleData, waveFormat, 0, sampleData.Length)
+        {
+        }
+
+        public SampleSource(float[] sampleData, WaveFormat waveFormat, int startIndex, int length)
+        {
+            this.SampleData = sampleData;
+            this.SampleWaveFormat = waveFormat;
+            this.StartIndex = startIndex;
+            this.Length = length;
+        }
+
+        /// <summary>
+        /// Sample data
+        /// </summary>
+        public float[] SampleData { get; private set; }
+        /// <summary>
+        /// Format of sampleData
+        /// </summary>
+        public WaveFormat SampleWaveFormat { get; private set; }
+        /// <summary>
+        /// Index of the first sample to play
+        /// </summary>
+        public int StartIndex { get; private set; }
+        /// <summary>
+        /// Number of valid samples
+        /// </summary>
+        public int Length { get; private set; }
+    }
+}

9eb5178d63101ab8842a9a4b97f3f1be156605cd
markheath <mark.heath@gmail.com>
2011-06-08 20:56:13 +0000

    Improved the visual look of the drum pattern editor, including note names

--- NAudioWpfDemo/DrumMachineDemo/DrumPatternEditor.xaml.cs
+++ NAudioWpfDemo/DrumMachineDemo/DrumPatternEditor.xaml.cs
@@ -22,12 +22,16 @@ namespace NAudioWpfDemo.DrumMachineDemo
     {
         private DrumPattern pattern;
         private double gridSquareWidth = 20;
+        private double namesColumnWidth = 100;
 
         public DrumPatternEditor()
         {
             InitializeComponent();
-            this.pattern = new DrumPattern(4, 16);
-            DrawPattern();
+            var notes = new string[] { "Kick", "Snare", "Closed Hats", "Open Hats" };
+            this.pattern = new DrumPattern(notes, 16);
+            DrawNoteNames();        
+            DrawPattern(namesColumnWidth);
+            DrawGridLines(namesColumnWidth);
         }
 
         public DrumPattern DrumPattern
@@ -35,45 +39,86 @@ namespace NAudioWpfDemo.DrumMachineDemo
             get { return pattern; }
         }
 
-        private void DrawPattern()
+        private void DrawNoteNames()
         {
-            
+            for (int note = 0; note < pattern.Notes; note++)
+            {
+                var tb = new TextBlock();
+                tb.Text = pattern.NoteNames[note];
+                tb.SetValue(Canvas.LeftProperty, 0.0);
+                tb.SetValue(Canvas.TopProperty, note * gridSquareWidth);
+                tb.Foreground = Brushes.Gray;
+                tb.FontFamily = new FontFamily("Segoe UI");
+                tb.FontSize = 12;
+                drumGridCanvas.Children.Add(tb);
+            }
+        }
+
+        private void DrawGridLines(double startX)
+        {
+            for (int step = 0; step <= pattern.Steps; step++)
+            {
+                // vertical lines
+                Line l = new Line();
+                l.X1 = l.X2 = startX + step * gridSquareWidth;
+                l.Y1 = 0;
+                l.Y2 = pattern.Notes * gridSquareWidth;
+                l.Stroke = step % 4 == 0 ? Brushes.Gray : Brushes.LightGray;
+                l.StrokeThickness = 1;
+                drumGridCanvas.Children.Add(l);
+            }
+
+            for (int note = 0; note <= pattern.Notes; note++)
+            {
+                Line l = new Line();
+                l.X1 = 0; // extend back to encompass names too
+                l.X2 = startX + pattern.Steps * gridSquareWidth;
+                l.Y1 = l.Y2 = note * gridSquareWidth;                
+                l.Stroke = Brushes.Gray;
+                l.StrokeThickness = 1;
+                drumGridCanvas.Children.Add(l);
+            }
+        }
+
+        private void DrawPattern(double startX)
+        {            
             for (int step = 0; step < pattern.Steps; step++)
             {
                 for (int note = 0; note < pattern.Notes; note++)
                 {
                     Rectangle r = new Rectangle();
-                    r.Stroke = Brushes.Black;
                     r.Fill = Brushes.White; // fill it or we won't get mouse-clicks
-                    r.StrokeThickness = 1;
                     r.Width = gridSquareWidth;
                     r.Height = gridSquareWidth;
-                    r.SetValue(Canvas.LeftProperty, step * gridSquareWidth);
+                    r.SetValue(Canvas.LeftProperty, startX + step * gridSquareWidth);
                     r.SetValue(Canvas.TopProperty, note * gridSquareWidth);
                     r.MouseLeftButtonUp += r_MouseLeftButtonUp;
                     //r.IsHitTestVisible = false;
-                    r.Tag = false;
+                    r.Tag = new PatternIndex(note, step);
                     drumGridCanvas.Children.Add(r);
                 }
             }
             //drumGridCanvas.MouseLeftButtonDown += r_MouseLeftButtonUp;
-
         }
 
         void r_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            /*Rectangle r = (Rectangle)sender;
-            bool isChecked = (bool)r.Tag;
-            r.Tag = !isChecked;
-            r.Fill = new SolidColorBrush(isChecked ? Colors.White : Colors.Red);*/
-            var p = e.GetPosition(drumGridCanvas);
-            int step = (int)(p.X / gridSquareWidth);
-            int note = (int)(p.Y / gridSquareWidth);
-            pattern[note, step] = pattern[note, step] == 0 ? (byte)127 : (byte)0;
             Rectangle r = (Rectangle)sender;
-            r.Fill = pattern[note, step] == 0 ? Brushes.White : Brushes.Red;
+            PatternIndex p = (PatternIndex)r.Tag;
+            
+            pattern[p.Note, p.Step] = pattern[p.Note, p.Step] == 0 ? (byte)127 : (byte)0;
+            r.Fill = pattern[p.Note, p.Step] == 0 ? Brushes.White : Brushes.LightSalmon;
+        }
+
+        class PatternIndex
+        {
+            public PatternIndex(int note, int step)
+            {
+                this.Note = note;
+                this.Step = step;
+            }
+            public int Note { get; private set; }
+            public int Step { get; private set; }
         }
     }
-
-
 }

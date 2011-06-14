915ad20d73d72f9229c5d38cb8ccf7547d03a58e
markheath <mark.heath@gmail.com>
2011-06-14 21:46:17 +0000

    Several fixes to the ACM structures (mainly packing fixes) to make them work when compiling in x64 mode. NAudioDemo now indicates whether it is running as x86 or x64 process.

--- NAudio/Wave/Compression/AcmDriverDetails.cs
+++ NAudio/Wave/Compression/AcmDriverDetails.cs
@@ -7,7 +7,7 @@ namespace NAudio.Wave.Compression
     /// Interop structure for ACM driver details (ACMDRIVERDETAILS)
     /// http://msdn.microsoft.com/en-us/library/dd742889%28VS.85%29.aspx
     /// </summary>
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack=2)]
     struct AcmDriverDetails
     {
         /// <summary>

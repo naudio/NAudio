cac2d21c485bd7ad1c16e4e0df300c1f4abbccf1
Mark Heath <mark.heath@gmail.com>
2012-10-06 11:40:49 +0100

    Improvements to NAudioDemo Recording Panel. Also WasapiCapture should be reusable now for same WaveFormat

--- NAudioDemo/RecordingDemo/RecordingPanel.Designer.cs
+++ NAudioDemo/RecordingDemo/RecordingPanel.Designer.cs
@@ -32,8 +32,6 @@
             this.buttonStartRecording = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonStopRecording = new System.Windows.Forms.Button();
-            this.checkBoxAutoPlay = new System.Windows.Forms.CheckBox();
-            this.buttonSelectOutputFile = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label2 = new System.Windows.Forms.Label();
             this.radioButtonWaveIn = new System.Windows.Forms.RadioButton();
@@ -41,6 +39,10 @@
             this.comboWasapiDevices = new System.Windows.Forms.ComboBox();
             this.radioButtonWasapiLoopback = new System.Windows.Forms.RadioButton();
             this.radioButtonWaveInEvent = new System.Windows.Forms.RadioButton();
+            this.listBoxRecordings = new System.Windows.Forms.ListBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonStartRecording
@@ -51,13 +53,15 @@
             this.buttonStartRecording.TabIndex = 0;
             this.buttonStartRecording.Text = "Start Recording";
             this.buttonStartRecording.UseVisualStyleBackColor = true;
-            this.buttonStartRecording.Click += new System.EventHandler(this.buttonStartRecording_Click);
+            this.buttonStartRecording.Click += new System.EventHandler(this.OnButtonStartRecordingClick);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Location = new System.Drawing.Point(12, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(396, 44);
+            this.label1.Size = new System.Drawing.Size(576, 44);
             this.label1.TabIndex = 1;
             this.label1.Text = resources.GetString("label1.Text");
             // 
@@ -69,33 +73,11 @@
             this.buttonStopRecording.TabIndex = 0;
             this.buttonStopRecording.Text = "Stop Recording";
             this.buttonStopRecording.UseVisualStyleBackColor = true;
-            this.buttonStopRecording.Click += new System.EventHandler(this.buttonStopRecording_Click);
-            // 
-            // checkBoxAutoPlay
-            // 
-            this.checkBoxAutoPlay.AutoSize = true;
-            this.checkBoxAutoPlay.Checked = true;
-            this.checkBoxAutoPlay.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxAutoPlay.Location = new System.Drawing.Point(237, 175);
-            this.checkBoxAutoPlay.Name = "checkBoxAutoPlay";
-            this.checkBoxAutoPlay.Size = new System.Drawing.Size(107, 17);
-            this.checkBoxAutoPlay.TabIndex = 2;
-            this.checkBoxAutoPlay.Text = "Play recorded file";
-            this.checkBoxAutoPlay.UseVisualStyleBackColor = true;
-            // 
-            // buttonSelectOutputFile
-            // 
-            this.buttonSelectOutputFile.Location = new System.Drawing.Point(15, 200);
-            this.buttonSelectOutputFile.Name = "buttonSelectOutputFile";
-            this.buttonSelectOutputFile.Size = new System.Drawing.Size(118, 23);
-            this.buttonSelectOutputFile.TabIndex = 3;
-            this.buttonSelectOutputFile.Text = "Select Output File...";
-            this.buttonSelectOutputFile.UseVisualStyleBackColor = true;
-            this.buttonSelectOutputFile.Click += new System.EventHandler(this.buttonSelectOutputFile_Click);
+            this.buttonStopRecording.Click += new System.EventHandler(this.OnButtonStopRecordingClick);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(15, 247);
+            this.progressBar1.Location = new System.Drawing.Point(257, 169);
             this.progressBar1.Maximum = 30;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(257, 23);
@@ -104,7 +86,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 231);
+            this.label2.Location = new System.Drawing.Point(254, 153);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 13);
             this.label2.TabIndex = 5;
@@ -118,6 +100,7 @@
             this.radioButtonWaveIn.Name = "radioButtonWaveIn";
             this.radioButtonWaveIn.Size = new System.Drawing.Size(60, 17);
             this.radioButtonWaveIn.TabIndex = 6;
+            this.radioButtonWaveIn.TabStop = true;
             this.radioButtonWaveIn.Text = "waveIn";
             this.radioButtonWaveIn.UseVisualStyleBackColor = true;
             // 
@@ -160,10 +143,57 @@
             this.radioButtonWaveInEvent.Text = "waveIn Event Callbacks";
             this.radioButtonWaveInEvent.UseVisualStyleBackColor = true;
             // 
+            // listBoxRecordings
+            // 
+            this.listBoxRecordings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxRecordings.FormattingEnabled = true;
+            this.listBoxRecordings.Location = new System.Drawing.Point(15, 211);
+            this.listBoxRecordings.Name = "listBoxRecordings";
+            this.listBoxRecordings.Size = new System.Drawing.Size(475, 95);
+            this.listBoxRecordings.TabIndex = 8;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlay.Location = new System.Drawing.Point(496, 211);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 9;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.OnButtonPlayClick);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.Location = new System.Drawing.Point(496, 240);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.OnButtonDeleteClick);
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenFolder.Location = new System.Drawing.Point(496, 269);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFolder.TabIndex = 10;
+            this.buttonOpenFolder.Text = "Open Folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.OnOpenFolderClick);
+            // 
             // RecordingPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonOpenFolder);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.listBoxRecordings);
             this.Controls.Add(this.comboWasapiDevices);
             this.Controls.Add(this.radioButtonWasapiLoopback);
             this.Controls.Add(this.radioButtonWasapi);
@@ -171,13 +201,11 @@
             this.Controls.Add(this.radioButtonWaveIn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.buttonSelectOutputFile);
-            this.Controls.Add(this.checkBoxAutoPlay);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonStopRecording);
             this.Controls.Add(this.buttonStartRecording);
             this.Name = "RecordingPanel";
-            this.Size = new System.Drawing.Size(420, 288);
+            this.Size = new System.Drawing.Size(600, 321);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,8 +216,6 @@
         private System.Windows.Forms.Button buttonStartRecording;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonStopRecording;
-        private System.Windows.Forms.CheckBox checkBoxAutoPlay;
-        private System.Windows.Forms.Button buttonSelectOutputFile;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RadioButton radioButtonWaveIn;
@@ -197,5 +223,9 @@
         private System.Windows.Forms.ComboBox comboWasapiDevices;
         private System.Windows.Forms.RadioButton radioButtonWasapiLoopback;
         private System.Windows.Forms.RadioButton radioButtonWaveInEvent;
+        private System.Windows.Forms.ListBox listBoxRecordings;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonOpenFolder;
     }
 }

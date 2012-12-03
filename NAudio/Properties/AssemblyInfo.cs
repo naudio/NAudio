21ced7c243941de7748cc3d06ce42a13e8e9e04a
Mark Heath <mark.heath@gmail.com>
2012-12-03 12:11:09 +0000

    moving MediaFoundationEncoder and MediaType into main NAudio dll. Important: this means we have finally made the conversion from .NET 2.0 to .NET 3.5 to allow LINQ and extension methods

--- NAudio/Properties/AssemblyInfo.cs
+++ NAudio/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.7.0.6")]
-[assembly: AssemblyFileVersion("1.7.0.6")]
+[assembly: AssemblyVersion("1.7.0.7")]
+[assembly: AssemblyFileVersion("1.7.0.7")]

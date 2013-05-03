1a72f56da5d308e396da0eaf641f4118343dfb32
Mark Heath <mark.heath@gmail.com>
2013-05-03 20:34:51 +0100

    nuget package now contains the Windows Store DLL (needs testing)

--- NAudio.Win8/Properties/AssemblyInfo.cs
+++ NAudio.Win8/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.5")]
-[assembly: AssemblyFileVersion("1.7.0.5")]
+[assembly: AssemblyVersion("1.7.0.11")]
+[assembly: AssemblyFileVersion("1.7.0.11")]
 [assembly: ComVisible(false)]

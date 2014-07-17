d642ff797252fdc02bb1c183bb93eb9bba91c000
Mark Heath <mark.heath@gmail.com>
2014-07-17 16:26:13 +0100

    attempting to fix ability to get the IAudioClock interface

--- NAudio/CoreAudioApi/Interfaces/IAudioClock2.cs
+++ NAudio/CoreAudioApi/Interfaces/IAudioClock2.cs
@@ -8,18 +8,18 @@ namespace NAudio.CoreAudioApi.Interfaces
     /// <summary>
     /// Defined in AudioClient.h
     /// </summary>
-    [Guid("6f49ff73-6727-49AC-A008-D98CF5E70048"),
+    [Guid("CD63314F-3FBA-4a1b-812C-EF96358728E7"),
         InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     internal interface IAudioClock
     {
-        [PreserveSig]
-        int GetCharacteristics(out uint characteristics);
-
         [PreserveSig]
         int GetFrequency(out ulong frequency);
 
         [PreserveSig]
         int GetPosition(out ulong devicePosition, out ulong qpcPosition);
+
+        [PreserveSig]
+        int GetCharacteristics(out uint characteristics);
     }
 
     /// <summary>

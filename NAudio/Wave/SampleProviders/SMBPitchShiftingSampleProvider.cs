0a9fe1a4415023a35b9d07cc9c6f1c7afb0e954e
Freefall63 <Omasreiter.Martin@gmail.com>
2016-09-28 22:23:37 +0200

    Update SMBPitchShiftingSampleProvider.cs
    Contains a simple limiter now

--- NAudio/Wave/SampleProviders/SMBPitchShiftingSampleProvider.cs
+++ NAudio/Wave/SampleProviders/SMBPitchShiftingSampleProvider.cs
@@ -16,9 +16,12 @@ using NAudio.Wave;
 /// For my needs the FastAttackCompressor1175 provides acceptable results:
 /// https://github.com/Jiyuu/SkypeFX/blob/master/JSNet/FastAttackCompressor1175.cs
 ///
+/// UPDATE: Added a simple Limiter based on the pydirac implementation.
+/// https://github.com/echonest/remix/blob/master/external/pydirac225/source/Dirac_LE.cpp
+/// 
 public class SMBPitchShiftingSampleProvider : ISampleProvider
 {
-
+    //Shifter objects
     private ISampleProvider SourceStream = null;
     private WaveFormat WFormat = null;
     private float Pitch = 1f;
@@ -26,9 +29,15 @@ public class SMBPitchShiftingSampleProvider : ISampleProvider
     private long _osamp;
     private SMBPitchShifter ShifterLeft = new SMBPitchShifter();
     private SMBPitchShifter ShifterRight = new SMBPitchShifter();
-    private float volscale = 1f; //Recommended to scale volume down, as SMB seems to clip with pitching
 
-    private object PitchLock = new object();
+    //Limiter constants
+    const float LIM_THRESH = 0.95f;
+    const float LIM_RANGE = (1f - LIM_THRESH);
+    const float M_PI_2 = (float)(Math.PI / 2);
+
+    public SMBPitchShiftingSampleProvider(ISampleProvider SourceProvider)
+        : this(SourceProvider, 4096, 4, 1f) { }
+        
     public SMBPitchShiftingSampleProvider(ISampleProvider SourceProvider, int FFTSize, long osamp, float InitialPitch)
     {
         SourceStream = SourceProvider;
@@ -40,46 +49,44 @@ public class SMBPitchShiftingSampleProvider : ISampleProvider
 
     public int Read(float[] buffer, int offset, int count)
     {
-        lock (PitchLock) {
-            int SampRead = SourceStream.Read(buffer, offset, count);
-            if (Pitch == 1f)    
-                //Nothing to do.
-                return SampRead;
-            if (WFormat.Channels == 1) {
-                float[] Mono = new float[SampRead];
-                int index = 0;
-                for (int sample = offset; sample <= SampRead - 1; sample++) {
-                    Mono[index] = buffer[sample];
-                    index += 1;
-                }
-                ShifterLeft.PitchShift(Pitch, SampRead, _FFTSize, _osamp, WFormat.SampleRate, Mono);
-                index = 0;
-                for (int sample = offset; sample <= SampRead - 1; sample++) {
-                    buffer[sample] = Mono[index] * volscale * 0.707f;
-                    index += 1;
-                }
-                return SampRead;
-            } else if (WFormat.Channels == 2) {
-                float[] Left = new float[(SampRead >> 1)];
-                float[] Right = new float[(SampRead >> 1)];
-                int index = 0;
-                for (int sample = offset; sample <= SampRead - 1; sample += 2) {
-                    Left[index] = buffer[sample];
-                    Right[index] = buffer[sample + 1];
-                    index += 1;
-                }
-                ShifterLeft.PitchShift(Pitch, SampRead >> 1, _FFTSize, _osamp, WFormat.SampleRate, Left);
-                ShifterRight.PitchShift(Pitch, SampRead >> 1, _FFTSize, _osamp, WFormat.SampleRate, Right);
-                index = 0;
-                for (int sample = offset; sample <= SampRead - 1; sample += 2) {
-                    buffer[sample] = Left[index] * volscale * 0.707f;
-                    buffer[sample + 1] = Right[index] * volscale * 0.707f;
-                    index += 1;
-                }
-                return SampRead;
-            } else {
-                throw new Exception("Shifting of more than 2 channels is currently not supported.");
+        int SampRead = SourceStream.Read(buffer, offset, count);
+        if (Pitch == 1f)    
+            //Nothing to do.
+            return SampRead;
+        if (WFormat.Channels == 1) {
+            float[] Mono = new float[SampRead];
+            int index = 0;
+            for (int sample = offset; sample <= SampRead - 1; sample++) {
+                Mono[index] = buffer[sample];
+                index += 1;
             }
+            ShifterLeft.PitchShift(Pitch, SampRead, _FFTSize, _osamp, WFormat.SampleRate, Mono);
+            index = 0;
+            for (int sample = offset; sample <= SampRead - 1; sample++) {
+                buffer[sample] = Limiter(Mono[index]);
+                index += 1;
+            }
+            return SampRead;
+        } else if (WFormat.Channels == 2) {
+            float[] Left = new float[(SampRead >> 1)];
+            float[] Right = new float[(SampRead >> 1)];
+            int index = 0;
+            for (int sample = offset; sample <= SampRead - 1; sample += 2) {
+                Left[index] = buffer[sample];
+                Right[index] = buffer[sample + 1];
+                index += 1;
+            }
+            ShifterLeft.PitchShift(Pitch, SampRead >> 1, _FFTSize, _osamp, WFormat.SampleRate, Left);
+            ShifterRight.PitchShift(Pitch, SampRead >> 1, _FFTSize, _osamp, WFormat.SampleRate, Right);
+            index = 0;
+            for (int sample = offset; sample <= SampRead - 1; sample += 2) {
+                buffer[sample] = Limiter(Left[index]);
+                buffer[sample + 1] = Limiter(Right[index]);
+                index += 1;
+            }
+            return SampRead;
+        } else {
+            throw new Exception("Shifting of more than 2 channels is currently not supported.");
         }
     }
 
@@ -90,20 +97,23 @@ public class SMBPitchShiftingSampleProvider : ISampleProvider
     public float PitchFactor {
         get { return Pitch; }
         set {
-            lock (PitchLock) {
-                Pitch = value;
-                ScaleVolume(); // A Limiter would be better than linear downscaling...
-            }
+            Pitch = value;
         }
     }
 
-    private void ScaleVolume()
+    private float Limiter(float Sample)
     {
-        if (Pitch > 1f) {
-            volscale = 1f / Pitch;
+        float res = 0f;
+        if ((LIM_THRESH < Sample)) {
+            res = (Sample - LIM_THRESH) / LIM_RANGE;
+            res = (float)((Math.Atan(res) / M_PI_2) * LIM_RANGE + LIM_THRESH);
+        } else if ((Sample < -LIM_THRESH)) {
+            res = -(Sample + LIM_THRESH) / LIM_RANGE;
+            res = -(float)((Math.Atan(res) / M_PI_2) * LIM_RANGE + LIM_THRESH);
         } else {
-            volscale = Pitch;
+            res = Sample;
         }
+        return res;
     }
 }
 

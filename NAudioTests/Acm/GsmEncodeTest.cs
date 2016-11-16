a00c2302a5e73c461abbcdaddfc8ac88bf6015e0
Mark Heath <mark.heath@gmail.com>
2016-11-16 23:08:50 +0000

    fixing a path issue with a unit test

--- NAudioTests/Acm/GsmEncodeTest.cs
+++ NAudioTests/Acm/GsmEncodeTest.cs
@@ -10,13 +10,19 @@ namespace NAudioTests.Acm
     [TestFixture]
     public class GsmEncodeTest
     {
+        private string tempFolder;
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            tempFolder = Path.Combine(Path.GetTempPath(), "NAudio");
+            if (!Directory.Exists(tempFolder))
+            {
+                Directory.CreateDirectory(tempFolder);
+            }
+        }
+
         private static WaveStream CreatePcmTestStream()
         {
-            var testFile = @"C:\Users\Mark\Code\CodePlex\pcm mono 16 bit 8kHz.wav";
-            if (File.Exists(testFile))
-            {
-                return new WaveFileReader(testFile);
-            }
             var outFormat = new WaveFormat(8000, 16, 1);
             const int durationInSeconds = 5;
             var sg = new SignalGenerator(outFormat.SampleRate, outFormat.Channels)
@@ -26,6 +32,8 @@ namespace NAudioTests.Acm
                 Type = SignalGeneratorType.Sin
             };
             var sp = sg.ToWaveProvider16();
+
+
             byte[] data = new byte[outFormat.AverageBytesPerSecond * durationInSeconds];
             var bytesRead = sp.Read(data, 0, data.Length);
             Assert.AreEqual(bytesRead, data.Length);
@@ -39,7 +47,7 @@ namespace NAudioTests.Acm
             {
                 using (var gsm = new WaveFormatConversionStream(new Gsm610WaveFormat(), reader))
                 {
-                    WaveFileWriter.CreateWaveFile(@"C:\Users\Mark\Code\CodePlex\gsm.wav", gsm);
+                    WaveFileWriter.CreateWaveFile(Path.Combine(tempFolder, "gsm.wav"), gsm);
                 }
             }
         }
@@ -47,16 +55,16 @@ namespace NAudioTests.Acm
         [Test]
         public void CanDecodeGsm()
         {
-            var testFile = @"C:\Users\Mark\Code\CodePlex\gsm.wav";
+            var testFile = Path.Combine(tempFolder, "gsm.wav");
             if (!File.Exists(testFile))
             {
-                Assert.Ignore("Missing test file");
+                Assert.Ignore("Missing test file (created by the another test)");
             }
             using (var reader = new WaveFileReader(testFile))
             {
                 using (var pcm = WaveFormatConversionStream.CreatePcmStream(reader))
                 {
-                    WaveFileWriter.CreateWaveFile(@"C:\Users\Mark\Code\CodePlex\gsm-decoded.wav", pcm);
+                    WaveFileWriter.CreateWaveFile(Path.Combine(tempFolder, "gsm-decoded.wav"), pcm);
                 }
             }
         }

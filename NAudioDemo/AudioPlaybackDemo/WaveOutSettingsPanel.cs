49268958b6d0f383434d28b653018eeb000b5d32
Mark Heath <mark.heath@gmail.com>
2017-03-10 22:41:16 +0000

    Allowing selecting of WaveOut device -1

--- NAudioDemo/AudioPlaybackDemo/WaveOutSettingsPanel.cs
+++ NAudioDemo/AudioPlaybackDemo/WaveOutSettingsPanel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using NAudio.Wave;
 
 namespace NAudioDemo.AudioPlaybackDemo
@@ -22,20 +20,18 @@ namespace NAudioDemo.AudioPlaybackDemo
                 Strategy = strategy;
             }
             public string Text { get; private set; }
-            public WaveCallbackStrategy Strategy { get; private set; }
+            public WaveCallbackStrategy Strategy { get; }
         }
 
         private void InitialiseDeviceCombo()
         {
-            for (int deviceId = 0; deviceId < WaveOut.DeviceCount; deviceId++)
+            if (WaveOut.DeviceCount <= 0) return;
+            for (var deviceId = -1; deviceId < WaveOut.DeviceCount; deviceId++)
             {
                 var capabilities = WaveOut.GetCapabilities(deviceId);
-                comboBoxWaveOutDevice.Items.Add(String.Format("Device {0} ({1})", deviceId, capabilities.ProductName));
+                comboBoxWaveOutDevice.Items.Add($"Device {deviceId} ({capabilities.ProductName})");
             }
-            if (comboBoxWaveOutDevice.Items.Count > 0)
-            {
-                comboBoxWaveOutDevice.SelectedIndex = 0;
-            }
+            comboBoxWaveOutDevice.SelectedIndex = 0;
         }
 
         private void InitialiseStrategyCombo()
@@ -48,8 +44,8 @@ namespace NAudioDemo.AudioPlaybackDemo
             comboBoxCallback.SelectedIndex = 0;
         }
 
-        public int SelectedDeviceNumber { get { return comboBoxWaveOutDevice.SelectedIndex; } }
+        public int SelectedDeviceNumber => comboBoxWaveOutDevice.SelectedIndex;
 
-        public WaveCallbackStrategy CallbackStrategy { get { return ((CallbackComboItem)comboBoxCallback.SelectedItem).Strategy; } }
+        public WaveCallbackStrategy CallbackStrategy => ((CallbackComboItem)comboBoxCallback.SelectedItem).Strategy;
     }
 }

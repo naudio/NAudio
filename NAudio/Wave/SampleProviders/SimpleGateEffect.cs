282fdd453898806bf000008161ec3c79ffe6404c
Jan Ivar Z. Carlsen <jizc@outlook.com>
2019-08-21 13:55:44 +0200

    Renamed field and ctor parameter

--- NAudio/Wave/SampleProviders/SimpleGateEffect.cs
+++ NAudio/Wave/SampleProviders/SimpleGateEffect.cs
@@ -7,7 +7,7 @@ namespace NAudio.Wave.SampleProviders
     /// </summary>
     public class SimpleGateEffect : ISampleProvider
     {
-        private readonly ISampleProvider sourceStream;
+        private readonly ISampleProvider sourceProvider;
         private readonly SimpleGate simpleGate;
         private readonly int channels;
         private readonly object lockObject = new object();
@@ -15,12 +15,12 @@ namespace NAudio.Wave.SampleProviders
         /// <summary>
         /// Create a new simple gate effect
         /// </summary>
-        /// <param name="sourceStream">Source stream</param>
-        public SimpleGateEffect(ISampleProvider sourceStream)
+        /// <param name="sourceProvider">Source sample provider</param>
+        public SimpleGateEffect(ISampleProvider sourceProvider)
         {
-            this.sourceStream = sourceStream;
-            channels = sourceStream.WaveFormat.Channels;
-            simpleGate = new SimpleGate(10.0, 100.0, sourceStream.WaveFormat.SampleRate);
+            this.sourceProvider = sourceProvider;
+            channels = sourceProvider.WaveFormat.Channels;
+            simpleGate = new SimpleGate(10.0, 100.0, sourceProvider.WaveFormat.SampleRate);
             simpleGate.Threshold = -50;
         }
 
@@ -77,7 +77,7 @@ namespace NAudio.Wave.SampleProviders
         /// <summary>
         /// Gets the WaveFormat of this stream
         /// </summary>
-        public WaveFormat WaveFormat => sourceStream.WaveFormat;
+        public WaveFormat WaveFormat => sourceProvider.WaveFormat;
 
         /// <summary>
         /// Reads bytes from this stream
@@ -90,7 +90,7 @@ namespace NAudio.Wave.SampleProviders
         {
             lock (lockObject)
             {
-                int samplesRead = sourceStream.Read(array, offset, count);
+                int samplesRead = sourceProvider.Read(array, offset, count);
                 if (Enabled)
                 {
                     for (int sample = 0; sample < samplesRead; sample += channels)

5b6cb25c8fdb493234c3771a05fb8533ac309bb3
John Wostenberg <jwosty@users.noreply.github.com>
2020-02-09 21:05:28 -0600

    MidiFileConverter now builds but crashes at runtime due to CLR version mismatches

--- MidiFileConverter/Properties/AssemblyInfo.cs
+++ MidiFileConverter/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MIDI File Converter")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mark Heath")]
-[assembly: AssemblyProduct("MIDI File Converter")]
-[assembly: AssemblyCopyright("Copyright © Mark Heath 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +9,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c25a6f6b-abf0-4460-a49e-d73c069f80e3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.3.11.0")]
-[assembly: AssemblyFileVersion("0.3.11.0")]
 
 // build 1 29 Oct 2006
 // build 1 is experimental

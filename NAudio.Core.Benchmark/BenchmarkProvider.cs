a1245c4883bcd644f69b54859fae8797e903e14c
macaba <macaba@gmail.com>
2021-06-12 22:19:54 +0100

    Added NAudio.Core Benchmark project

---
+++ NAudio.Core.Benchmark/BenchmarkProvider.cs
@@ -0,0 +1,24 @@
+﻿using NAudio.Wave;
+
+namespace NAudio.Core.Benchmark
+{
+    public class BenchmarkProvider : ISampleProvider, IWaveProvider
+    {
+        public WaveFormat WaveFormat { get; private set; }
+
+        public BenchmarkProvider(WaveFormat waveFormat)
+        {
+            WaveFormat = waveFormat;
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            return count;
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return count;
+        }
+    }
+}
---
+++ NAudio.Core.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace NAudio.Core.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Wave.SampleProviders.WaveToSampleProviderBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}

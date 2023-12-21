d994add517dbd5a8545a799ba961e7b3fdd557cb
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-12-21 19:29:33 +0100

    Don't write extraSize WAV-header field for PCM data
    fixes #934

--- NAudio.Core/Wave/WaveFormats/WaveFormat.cs
+++ NAudio.Core/Wave/WaveFormats/WaveFormat.cs
@@ -302,14 +302,24 @@ namespace NAudio.Wave
         /// <param name="writer">the output stream</param>
         public virtual void Serialize(BinaryWriter writer)
         {
-            writer.Write((int)(18 + extraSize)); // wave format length
+            if(Encoding == WaveFormatEncoding.Pcm)
+            {
+                writer.Write((int)(16)); // wave format length
+            }
+            else
+            {
+                writer.Write((int)(18 + extraSize)); // wave format length
+            }
             writer.Write((short)Encoding);
             writer.Write((short)Channels);
             writer.Write((int)SampleRate);
             writer.Write((int)AverageBytesPerSecond);
             writer.Write((short)BlockAlign);
             writer.Write((short)BitsPerSample);
-            writer.Write((short)extraSize);
+            if(Encoding != WaveFormatEncoding.Pcm)
+            {
+                writer.Write((short)extraSize);
+            }
         }
 
         /// <summary>

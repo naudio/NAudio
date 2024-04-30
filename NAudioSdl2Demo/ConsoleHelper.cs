d4a0c0fc87d03e28de10b836da7c4e61b7471803
trifonovaa <TrifonovAA@umssoft.com>
2024-04-30 10:49:09 +0700

    field style

--- NAudioSdl2Demo/ConsoleHelper.cs
+++ NAudioSdl2Demo/ConsoleHelper.cs
@@ -2,18 +2,18 @@
 {
     public static class ConsoleHelper
     {
-        private static Dictionary<string, (int, int)?> _cursorPositions;
+        private static readonly Dictionary<string, (int, int)?> cursorPositions;
 
         static ConsoleHelper()
         {
-            _cursorPositions = new Dictionary<string, (int, int)?>();
+            cursorPositions = new Dictionary<string, (int, int)?>();
         }
 
         public static void LockCursorPosition(string key)
         {
-            _cursorPositions.TryAdd(key, null);
-            _cursorPositions[key] ??= Console.GetCursorPosition();
-            Console.SetCursorPosition(_cursorPositions[key]!.Value.Item1, _cursorPositions[key]!.Value.Item2);
+            cursorPositions.TryAdd(key, null);
+            cursorPositions[key] ??= Console.GetCursorPosition();
+            Console.SetCursorPosition(cursorPositions[key]!.Value.Item1, cursorPositions[key]!.Value.Item2);
         }
     }
 }
